a215e563726689f5da871fd66da24b204787ceff
EmmanuelEzekielSolomon <emmanuel.solomon@crestagile.com>
2025-04-13 20:03:30 +0100

    added base dir

--- spabusmgt/settings.py
+++ spabusmgt/settings.py
@@ -16,11 +16,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 TEMPLATE_DIR =os.path.join(BASE_DIR, 'templates')
 
 
