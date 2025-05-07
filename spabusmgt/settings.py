26402988816df6eb7bf4e84c9024a1fcd1dbe14d
EmmanuelEzekielSolomon <emmanuel.solomon@crestagile.com>
2025-05-07 11:38:39 +0100

    added whitenoise

--- spabusmgt/settings.py
+++ spabusmgt/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'whitenoise.runserver_nostatic',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
